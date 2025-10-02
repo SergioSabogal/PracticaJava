3886648ea4b65f262c70102d9bf1c1aebfd70a0a
Sergio <sergiosabogal1911@gmail.com>
2025-10-02 11:08:07 -0500

    Tercer ejercicio

---
+++ src/Ejercicio3.java
@@ -0,0 +1,20 @@
+public class Ejercicio3 {
+    /*
+     * Escribe un programa que imprima los 50 primeros números de la sucesión
+     * de Fibonacci empezando en 0.
+     * - La serie Fibonacci se compone por una sucesión de números en
+     *   la que el siguiente siempre es la suma de los dos anteriores.
+     *   0, 1, 1, 2, 3, 5, 8, 13...
+     */
+
+    public static void main(String[] args) {
+        int suma = 0;
+        int anterior = 0;
+        int init = 1;
+        for(int i = 0; i < 50 ; i++){
+
+
+            System.out.println(suma);
+        }
+    }
+}
