a04424d29666e3a979d311001c246cf23459c802
Sergio <sergiosabogal1911@gmail.com>
2025-10-03 09:10:36 -0500

    Desarrollo ejercicio 4, numero primo

---
+++ src/Ejercicio4.java
@@ -0,0 +1,21 @@
+public class Ejercicio4 {
+    /*
+     * Escribe un programa que se encargue de comprobar si un número es o no primo.
+     * Hecho esto, imprime los números primos entre 1 y 100.
+     */
+    public static void main(String[] args) {
+        for( int i = 1 ; i<=100 ; i++){
+            int contador = 0;
+            for(int j = 1 ; j <= i  ; j++){
+                if(i%j == 0 ){
+                    contador++;
+
+                }
+            }
+            if(contador <= 2 ){
+                System.out.println(i);
+            }
+        }
+    }
+
+}
